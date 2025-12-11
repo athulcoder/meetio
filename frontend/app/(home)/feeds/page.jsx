a4e6ec34f0e9ef3fd50530bbd568b5797d7d5972
athulcoder <athul.sin.cos.tan@gmail.com>
2025-12-11 14:42:02 +0530

    made the feed section on center

--- frontend/app/(home)/feeds/page.jsx
+++ frontend/app/(home)/feeds/page.jsx
@@ -5,18 +5,18 @@ import React from "react";
 
 const FeedPage = () => {
   return (
-    <section className="bg-background h-screen w-full  overflow-y-auto   ">
+    <section className="bg-background h-screen w-full  overflow-y-auto  ">
       {/* Story section */}
       <StorySection />
       <hr className="border-[0.2px] border-[#c7c7c7]" />
 
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-
-      <div className="w-full bg-bluish-white  h-[80px]"></div>
+      <div className="flex items-center ml-[-300px]   flex-col w-full">
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
     </section>
   );
 };
