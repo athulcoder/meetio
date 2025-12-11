96b8862cf13039bcd6288b099c04213ec90dbc88
athulcoder <athul.sin.cos.tan@gmail.com>
2025-12-11 14:53:29 +0530

    bottom padding issue fixed

--- frontend/app/(home)/feeds/page.jsx
+++ frontend/app/(home)/feeds/page.jsx
@@ -5,12 +5,12 @@ import React from "react";
 
 const FeedPage = () => {
   return (
-    <section className="bg-background h-screen w-full  overflow-y-auto  ">
+    <section className="bg-background h-screen w-full  overflow-y-auto pb-30 ">
       {/* Story section */}
       <StorySection />
       <hr className="border-[0.2px] border-[#c7c7c7]" />
 
-      <div className="flex items-center min-md:items-start min-md:ml-10    flex-col w-full">
+      <div className="w-full flex flex-col items-center min-md:ml-10 min-md:items-start ">
         <PostCard />
         <PostCard />
         <PostCard />
